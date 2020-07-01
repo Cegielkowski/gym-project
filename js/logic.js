1974ac9bd87fe0a28bfa0b1b779b0e04c007728a
Lucas Cegielkowski Guimaraes <cegielkowski@MacBook-Pro.local>
2020-06-30 23:36:06 -0300

    feito agendamento e usuario

--- js/logic.js
+++ js/logic.js
@@ -1,12 +1,12 @@
 $(function(){});
-var select = $('#type-person');
+var select = $('#tipo');
 
 select.change(function(){
     if (select.val() == 'personal') {
         $('#for-chicken').hide();
         $('#for-personal').show('slow');
     }
-    if (select.val() == 'chicken') {
+    if (select.val() == 'aluno') {
         $('#for-personal').hide();
         $('#for-chicken').show('slow');
     }
